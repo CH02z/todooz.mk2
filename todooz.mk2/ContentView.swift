3e02c8dc5b40d6889181e7396ce5d1d8d1594e28
Chris Zimmermann <chris.zimmermann@hotmail.ch>
2023-09-22 15:05:13 +0200

    Initial Commit

---
+++ todooz.mk2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  todooz.mk2
+//
+//  Created by Chris Zimmermann on 22.09.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ todooz.mk2/todooz_mk2App.swift
@@ -0,0 +1,17 @@
+//
+//  todooz_mk2App.swift
+//  todooz.mk2
+//
+//  Created by Chris Zimmermann on 22.09.23.
+//
+
+import SwiftUI
+
+@main
+struct todooz_mk2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
